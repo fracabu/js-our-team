0c74b5dd25875b1753e8855148bc46f395dc5222
fracabu <100199965+fracabu@users.noreply.github.com>
2022-06-06 22:27:09 +0200

    stampo nome e ruolo

--- script.js
+++ script.js
@@ -14,10 +14,18 @@ membro del team un elemento html che conterrà i suoi dati.
 // definire un array di oggetti che rappresentano i membri del team
 
 const elencoMembriTeam = [
+
+    { 
+        nome: "Wayne Barnett",
+        ruolo: "Founder CEO",
+        foto:"img/wayne-barnett-founder-ceo.jpg",
+
+    },
+
     { 
         nome: "Angela Caroll",
         ruolo: "Chief Editor",
-        foto:"img",
+        foto:"img/angela-caroll-chief-editor.jpg",
 
     },
 
@@ -25,28 +33,28 @@ const elencoMembriTeam = [
     { 
         nome: "Walter Gordon",
         ruolo: "Office Manager",
-        foto: "img",
+        foto: "img/walter-gordon-office-manager.jpg",
 
     },
 
     { 
         nome: "Angela Lopez",
         ruolo: "Social Media Manager",
-        foto: "img",
+        foto: "img/angela-lopez-social-media-manager.jpg",
 
     },
 
     { 
         nome: "Scott Estrada",
         ruolo: "Developer",
-        foto: "img",
+        foto: "img/scott-estrada-developer.jpg",
 
     },
 
     { 
         nome: "Barbara Ramos",
         ruolo: "Graphic Designer",
-        foto: "img",
+        foto: "img/barbara-ramos-graphic-designer.jpg",
 
     },
     
@@ -55,10 +63,15 @@ console.log(elencoMembriTeam);
 
 
 //Stampo in console l'array di oggetti creati
-
+/*
 for (let key in elencoMembriTeam) {
     let card = elencoMembriTeam[key];
     console.log(card);
     console.log(card.nome);
 }
-
+*/
+for (let i = 0; i < elencoMembriTeam.length; i++){
+    const teamMember = elencoMembriTeam[i]; // teamMember = oggetto con chiavi nome ruolo e img
+    //stampo nome e ruolo di ogni membro
+    console.log(`Nome: ${teamMember.nome} - Ruolo: ${teamMember.ruolo}`);
+}
